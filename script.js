3817b3ebc84169f71d86e5ae7dd832f95ef84abb
p1zzakat <rbanine@gmail.com>
2025-07-15 17:13:54 +0100

    alpha version, minor bugs that need to be fixed and styling still needs to be done, I know these commit messages are trash but I keep forgetting to commit, I need a commit reminder

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const currentLibrary = document.querySelector(".library");
+const addBookButton = document.querySelector(".button.add");
+const clearFormButton = document.querySelector(".button.clear");
+const inputName = document.querySelector("#name");
+const inputAuthor = document.querySelector("#author");
+const inputNumberOfPages = document.querySelector("#number-of-pages");
+const inputIsRead = document.querySelector("#is-read");
+
 const myLibrary = [];
+
+const book1 = new Book("Hacker's Delight", "Henry S. Warren", 306, false);
+const book2 = new Book(
+    "The Art of Computer Programming",
+    "Donald Knuth",
+    672,
+    false
+);
+
+myLibrary.push(book1);
+myLibrary.push(book2);
 
 function Book(name, author, pages, isRead) {
     this.name = name;
@@ -9,12 +27,8 @@ function Book(name, author, pages, isRead) {
     this.id = crypto.randomUUID();
 }
 
-function addBookToLibrary(name, author, pages, isRead, myLibrary) {
-    const book = new Book(name, author, pages, isRead);
-    myLibrary.push(book);
-}
-
 function displayLibrary(library) {
+    clearLibrary();
     library.forEach((book) => {
         const bookElement = createBookElement(book);
         currentLibrary.appendChild(bookElement);
@@ -33,22 +47,66 @@ function createBookElement(book) {
     const pagesElement = document.createElement("h4");
     pagesElement.textContent = `Number Of Pages: ${book.pages}`;
     bookElement.appendChild(pagesElement);
-    const isreadElement = document.createElement("h4");
-    isreadElement.textContent = `Did you read it yet?: ${book.isRead}`;
-    bookElement.appendChild(isreadElement);
+    const isReadElement = document.createElement("h4");
+    isReadElement.textContent = `Did you read it yet? ${book.isRead ? 'yes' : 'no'}`
+    bookElement.appendChild(isReadElement);
+    const removeSelfButton = document.createElement("button");
+    removeSelfButton.textContent = "Remove";
+    removeSelfButton.classList.add("button", "remove");
+    removeSelfButton.dataset.id = book.id;
+    removeSelfButton.addEventListener("click", removeBookFromLibrary);
+    bookElement.appendChild(removeSelfButton);
+    const changeStatusButton = document.createElement("button");
+    changeStatusButton.textContent = "Change Status";
+    changeStatusButton.classList.add("button", "change-status");
+    changeStatusButton.dataset.id = book.id;
+    changeStatusButton.addEventListener("click", changeStatus);
+    bookElement.appendChild(changeStatusButton);
+    book.bookElement = bookElement;
 
     return bookElement;
 }
 
-const book1 = new Book("Hacker's Delight", "Henry S. Warren", 306, false);
-const book2 = new Book(
-    "The Art of Computer Programming",
-    "Donald Knuth",
-    672,
-    false
-);
+function addBookToLibrary() {
+    const name = inputName.value;
+    const author = inputAuthor.value;
+    const numberOfPages = inputNumberOfPages.value;
+    const isRead = inputIsRead.checked;
+    const book = new Book(name, author, numberOfPages, isRead);
+    myLibrary.push(book);
+    displayLibrary(myLibrary);
+}
 
-myLibrary.push(book1);
-myLibrary.push(book2);
+function clearLibrary() {
+    currentLibrary.innerHTML = "";
+}
+
+function clearForm() {
+    inputName.value = "";
+    inputAuthor.value = "";
+    inputNumberOfPages.value = "";
+    inputIsRead.value = false;
+}
+
+function removeBookFromLibrary(event) {
+    const bookId = event.target.dataset.id;
+    for (book of myLibrary) {
+        if (book.id === bookId) currentLibrary.removeChild(book.bookElement);
+    }
+}
+
+function changeStatus(event) {
+    const bookId = event.target.dataset.id;
+    for (book of myLibrary) {
+        if (book.id === bookId) {
+            book.isRead ? book.isRead = false : book.isRead = true;
+            displayLibrary(myLibrary);
+        }
+    }
+}
 
 displayLibrary(myLibrary);
+
+addBookButton.addEventListener("click", addBookToLibrary);
+
+clearFormButton.addEventListener("click", clearForm);
